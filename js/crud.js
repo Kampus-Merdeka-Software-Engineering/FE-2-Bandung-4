a20d34221b34b7cb1a76c0f433fb50a7f9abb627
Sahrul Riswanto <Shrl.riswanto@gmail.com>
2023-11-16 21:11:54 +0700

    menambahkan pencarian tiket

--- js/crud.js
+++ js/crud.js
@@ -1,25 +1,29 @@
-var data = []; // Array untuk menyimpan data pemesanan
-
 function submitForm() {
-  var nama = document.getElementById("nama").value;
-  var maskapai = document.getElementById("maskapai").value;
-  var tanggal = document.getElementById("tanggal").value;
-  var asal = document.getElementById("asal").value;
-  var tujuan = document.getElementById("tujuan").value;
+  const nama = document.getElementById("nama").value;
+  const maskapai = document.getElementById("maskapai").value;
+  const tanggal = document.getElementById("tanggal").value;
+  const asal = document.getElementById("asal").value;
+  const tujuan = document.getElementById("tujuan").value;
 
-  var kodeBooking = generateKodeBooking();
+  // Memeriksa apakah ada input yang kosong
+  if (!nama || !maskapai || !tanggal || !asal || !tujuan) {
+    alert("Semua kolom harus diisi!");
+    return;
+  }
+
+  const kodeBooking = generateKodeBooking();
 
   // Memeriksa apakah ini operasi Create atau Update
-  var dataId = document.getElementById("dataId").value;
+  const dataId = document.getElementById("dataId").value;
   if (dataId === "") {
     // Create
-    var newData = {
-      nama: nama,
-      maskapai: maskapai,
-      tanggal: tanggal,
-      asal: asal,
-      tujuan: tujuan,
-      kodeBooking: kodeBooking,
+    const newData = {
+      nama,
+      maskapai,
+      tanggal,
+      asal,
+      tujuan,
+      kodeBooking,
     };
 
     data.push(newData);
@@ -38,51 +42,4 @@ function submitForm() {
   // Mereset form
   document.getElementById("bookingForm").reset();
   document.getElementById("dataId").value = "";
-}
-
-function generateKodeBooking() {
-  var timestamp = new Date().getTime();
-  return "BK" + timestamp;
-}
-
-function renderTable() {
-  var tableBody = document.getElementById("data-body");
-  tableBody.innerHTML = "";
-
-  for (var i = 0; i < data.length; i++) {
-    var row = tableBody.insertRow(i);
-    row.insertCell(0).innerHTML = data[i].nama;
-    row.insertCell(1).innerHTML = data[i].maskapai;
-    row.insertCell(2).innerHTML = data[i].tanggal;
-    row.insertCell(3).innerHTML = data[i].asal;
-    row.insertCell(4).innerHTML = data[i].tujuan;
-    row.insertCell(5).innerHTML = data[i].kodeBooking;
-
-    // Tombol Edit dan Delete
-    var cell = row.insertCell(6);
-    cell.innerHTML =
-      "<button onclick='editData(" +
-      i +
-      ")'>Edit</button> <button onclick='deleteData(" +
-      i +
-      ")'>Delete</button>";
-  }
-}
-
-function editData(index) {
-  // Mengisi form dengan data yang akan diubah
-  document.getElementById("nama").value = data[index].nama;
-  document.getElementById("maskapai").value = data[index].maskapai;
-  document.getElementById("tanggal").value = data[index].tanggal;
-  document.getElementById("asal").value = data[index].asal;
-  document.getElementById("tujuan").value = data[index].tujuan;
-  document.getElementById("dataId").value = index;
-}
-
-function deleteData(index) {
-  // Menghapus data dari array
-  data.splice(index, 1);
-
-  // Memperbarui tabel
-  renderTable();
 }
--- js/index.js
+++ js/index.js
@@ -25,3 +25,50 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("aktif");
   }
 });
+
+// Menggunakan const dan let untuk deklarasi variabel
+function cariTiket() {
+  // Dapatkan nilai dari input form
+  const asal = document.getElementById("asal").value;
+  const tujuan = document.getElementById("tujuan").value;
+  const tanggal = document.getElementById("tanggal").value;
+  const maskapai = document.getElementById("Maskapai").value;
+
+  // Lakukan validasi
+  if (!asal || !tujuan || !tanggal || !maskapai) {
+    alert("Semua kolom harus diisi!");
+    return;
+  }
+
+  // Lakukan logika pencarian (Anda bisa memodifikasi ini sesuai kebutuhan)
+  const hasilPencarian = cariTiketDariServer(asal, tujuan, tanggal, maskapai);
+
+  // Tampilkan hasil pencarian di elemen hasilPencarian
+  tampilkanHasilPencarian(hasilPencarian);
+}
+
+// Menggunakan arrow function
+function cariTiketDariServer(asal, tujuan, tanggal, maskapai) {
+  const hasilPencarian = [
+    { maskapai: "Garuda", harga: 1000000, waktu: "08:00" },
+    { maskapai: "Lion Air", harga: 800000, waktu: "10:00" },
+  ];
+
+  return hasilPencarian;
+}
+
+// Menggunakan template literals dan forEach
+function tampilkanHasilPencarian(hasilPencarian) {
+  const hasilPencarianElem = document.getElementById("hasilPencarian");
+  hasilPencarianElem.innerHTML = ""; // Bersihkan hasil pencarian sebelumnya
+
+  // Tambahkan hasil pencarian ke elemen
+  const ul = document.createElement("ul");
+  hasilPencarian.forEach((tiket) => {
+    const li = document.createElement("li");
+    li.textContent = `${tiket.maskapai} - Harga: ${tiket.harga} - Waktu: ${tiket.waktu}`;
+    ul.appendChild(li);
+  });
+
+  hasilPencarianElem.appendChild(ul);
+}
