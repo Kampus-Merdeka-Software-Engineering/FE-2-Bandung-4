267efce1e4606c3c9a3ec60253256d0f694e4fad
Sahrul Riswanto <Shrl.riswanto@gmail.com>
2023-11-23 13:56:09 +0700

    tes

--- js/crud.js
+++ js/crud.js
@@ -39,23 +39,6 @@ function submitForm() {
   // Memperbarui tabel
   renderTable();
 
-  // Menampilkan data yang baru ditambahkan
-  const table = document.getElementById("dataTable");
-  const newRow = table.insertRow(-1);
-  const cellNama = newRow.insertCell(0);
-  const cellMaskapai = newRow.insertCell(1);
-  const cellTanggal = newRow.insertCell(2);
-  const cellAsal = newRow.insertCell(3);
-  const cellTujuan = newRow.insertCell(4);
-  const cellKodeBooking = newRow.insertCell(5);
-
-  cellNama.innerHTML = nama;
-  cellMaskapai.innerHTML = maskapai;
-  cellTanggal.innerHTML = tanggal;
-  cellAsal.innerHTML = asal;
-  cellTujuan.innerHTML = tujuan;
-  cellKodeBooking.innerHTML = kodeBooking;
-
   // Mereset form
   document.getElementById("bookingForm").reset();
   document.getElementById("dataId").value = "";
