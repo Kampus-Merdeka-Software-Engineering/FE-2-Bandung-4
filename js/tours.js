36163a3fdbb5a89aa6b6753774e4c4e51783257d
Sahrul Riswanto <Shrl.riswanto@gmail.com>
2023-11-24 19:23:31 +0700

    membuat file detail.html

--- js/tours.js
+++ js/tours.js
@@ -1,17 +1,34 @@
+// Mengubah format harga menjadi rupiah
+const formatRupiah = (price) => {
+  // Menggunakan Intl.NumberFormat untuk mengonversi harga ke format rupiah
+  const formattedPrice = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(price);
+
+  return formattedPrice;
+};
+
+// Menduplikasi array products ke filteredProducts
 let filteredProducts = [...products];
 
+// Mengambil elemen DOM untuk menampilkan produk
 const productsContainer = document.querySelector(".products-container");
 
+// Fungsi untuk menampilkan produk di dalam DOM
 const displayProducts = () => {
+  // Menampilkan pesan jika tidak ada produk yang sesuai dengan pencarian
   if (filteredProducts.length < 1) {
     productsContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
     return;
   }
 
+  // Mengisi kontainer produk dengan HTML hasil mapping dari filteredProducts
   productsContainer.innerHTML = filteredProducts
     .map((product) => {
       const { id, title, image, price } = product;
-      return `<article class="product" data-id="${id}">
+      return `<a href="detail.html?id=${id}">
+      <article class="product" data-id="${id}">
           <img
             src="${image}"
             class="product-img img"
@@ -19,44 +36,43 @@ const displayProducts = () => {
           />
           <footer>
             <h5 class="product-name">${title}</h5>
-            <span class="product-price">${price}</span>
+            <!-- Menampilkan harga dalam format rupiah -->
+            <span class="product-price">${formatRupiah(price)}</span>
           </footer>
-        </article>`;
+        </article></a>`;
     })
     .join("");
 };
 
+// Memanggil fungsi displayProducts untuk menampilkan produk awal
 displayProducts();
 
-// Text Filter
-
+// Event listener untuk input pencarian
 const form = document.querySelector(".input-form");
 const searchInput = document.querySelector(".search-input");
 
 form.addEventListener("keyup", () => {
+  // Mendapatkan nilai input pencarian
   const inputValue = searchInput.value;
+  // Memfilter produk berdasarkan judul sesuai dengan input pencarian
   filteredProducts = products.filter((product) => {
     return product.title.toLowerCase().includes(inputValue);
   });
+  // Menampilkan produk yang sesuai dengan filter
   displayProducts();
 });
 
-// console.log(
-//   products.filter((product) => {
-//     return product.title.toLowerCase().includes('');
-//   })
-// );
-
-// Filter Buttons
-
+// Mengambil elemen DOM untuk menampilkan tombol perusahaan
 const companiesDOM = document.querySelector(".companies");
 
+// Fungsi untuk menampilkan tombol perusahaan di dalam DOM
 const displayButtons = () => {
+  // Mengambil daftar unik perusahaan dari produk
   const buttons = [
     "all",
     ...new Set(products.map((product) => product.company)),
   ];
-  // console.log(buttons);
+  // Menampilkan tombol perusahaan berdasarkan daftar unik
   companiesDOM.innerHTML = buttons
     .map((company) => {
       return `<button class='company-btn' data-id="${company}">${company}</button>`;
@@ -64,10 +80,13 @@ const displayButtons = () => {
     .join("");
 };
 
+// Memanggil fungsi displayButtons untuk menampilkan tombol perusahaan awal
 displayButtons();
 
+// Event listener untuk klik tombol perusahaan
 companiesDOM.addEventListener("click", (e) => {
   const el = e.target;
+  // Memfilter produk berdasarkan perusahaan sesuai dengan tombol yang diklik
   if (el.classList.contains("company-btn")) {
     if (el.dataset.id === "all") {
       filteredProducts = [...products];
@@ -76,7 +95,9 @@ companiesDOM.addEventListener("click", (e) => {
         return product.company === el.dataset.id;
       });
     }
+    // Mengosongkan nilai input pencarian
     searchInput.value = "";
+    // Menampilkan produk yang sesuai dengan filter
     displayProducts();
   }
 });
