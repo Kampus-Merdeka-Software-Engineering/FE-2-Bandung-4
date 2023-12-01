ab8d23051bc67bea157472090a430a076e3e4758
Sahrul Riswanto <Shrl.riswanto@gmail.com>
2023-12-01 16:36:20 +0700

    menambah tentang kami

--- js/script.js
+++ js/script.js
@@ -68,6 +68,25 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("An error occurred:", error);
   }
 });
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
 
 // ===============
 // Data
